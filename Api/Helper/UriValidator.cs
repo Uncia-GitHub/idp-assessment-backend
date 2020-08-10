de2d7a52d2a3409187e7aa6d09c3aedc5eacd8b7
maryna.banova <maryna.banova@gmail.com>
2020-08-10 22:06:50 +0200

    implementation uriValadator

--- Api/Helper/UriValidator.cs
+++ Api/Helper/UriValidator.cs
@@ -1,16 +1,33 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Api.Helper
 {
     public class UriValidator
     {
-        public UriValidator()
-        {
-        }
-
         public bool Validate(string uri, string[] validationRules)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(uri))
+                throw new System.ArgumentException("Url cannot be null");
+
+            var rules = validationRules.Select(x => WildcardToRegex(x));
+
+            foreach (string rule in rules)
+            {
+                if (Regex.IsMatch(uri, rule, RegexOptions.IgnoreCase))
+                    return true;
+
+            }
+            return false;
+        }
+
+        private string WildcardToRegex(string pattern)
+        {
+
+            return Regex.Escape(pattern).
+            Replace("\\*", ".*").
+            Replace("\\?", ".");
         }
     }
 }
--- AssessmentTests/UriValidatorTests.cs
+++ AssessmentTests/UriValidatorTests.cs
@@ -16,8 +16,11 @@ namespace AssessmentTests
 
 
         [TestMethod]
-        public void UriValidator_AbsoluteUri()
+        public void UriValidator_AbsoluteUri_true()
         {
+            string[] validationRules = new[]{
+           "https://www.confirmit.com/webapp/logincallback" // absolute rule - this url is allowed
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://www.confirmit.com/webapp/logincallback", validationRules); // expected true
@@ -26,8 +29,24 @@ namespace AssessmentTests
         }
 
         [TestMethod]
+        public void UriValidator_AbsoluteUri_false()
+        {
+            string[] validationRules = new[]{
+           "https://www.confirmit.com/webapp/logincallback" // absolute rule - this url is allowed
+           };
+
+            var validator = new UriValidator();
+            var isValid = validator.Validate("https://www.confirmit.com/app/logincallback", validationRules); // expected false
+
+            Assert.AreEqual(false, isValid);
+        }
+
+        [TestMethod]
         public void UriValidator_wildcardUri_true()
         {
+            string[] validationRules = new[]{
+           "https://*.confirmit.com/*/login" 
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://www.confirmit.com/webapp/login", validationRules); // expected true
@@ -38,6 +57,9 @@ namespace AssessmentTests
         [TestMethod]
         public void UriValidator_wildcardUri2_wrongDomain_false()
         {
+            string[] validationRules = new[]{
+           "https://*.confirmit.com/webapp/login"
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://www.confirmit.net/webapp/login", validationRules); // expected false
@@ -49,6 +71,9 @@ namespace AssessmentTests
         [TestMethod]
         public void UriValidator_wildcardUri3_withFolder_true()
         {
+            string[] validationRules = new[]{
+           "https://*.confirmit.com/*/logincallback"
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://login.confirmit.com/app/logincallback", validationRules); // expected true
@@ -59,6 +84,9 @@ namespace AssessmentTests
         [TestMethod]
         public void UriValidator_wildcardUri4_withFolder1_true()
         {
+            string[] validationRules = new[]{
+           "https://*.confirmit.com/*/logincallback"
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://auth.confirmit.com/login/logincallback", validationRules); // expected true
@@ -69,6 +97,9 @@ namespace AssessmentTests
         [TestMethod]
         public void UriValidator_wildcardUri_pathWithSeveralFolder_true()
         {
+            string[] validationRules = new[]{
+           "https://*.confirmit.com/*/logincallback"
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://app.confirmit.com/auth/folder/logincallback", validationRules); // expected true
@@ -79,6 +110,9 @@ namespace AssessmentTests
         [TestMethod]
         public void UriValidator_wildcardUri_withoutSubdomen_false()
         {
+            string[] validationRules = new[]{
+           "https://*.confirmit.com/*/logincallback"
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://confirmit.com/logincallback", validationRules); // expected false
@@ -89,6 +123,9 @@ namespace AssessmentTests
         [TestMethod]
         public void UriValidator_literalRegexUri1_withFolder_true()
         {
+            string[] validationRules = new[]{
+           @"@https://([\w-]+\.)*confirmit.com/logincallback"
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://login.confirmit.com/app/logincallback", validationRules); // expected true
@@ -99,6 +136,9 @@ namespace AssessmentTests
         [TestMethod]
         public void UriValidator_literalRegexUri2_withFolder1_true()
         {
+            string[] validationRules = new[]{
+           @"@https://([\w-]+\.)confirmit.com/login/logincallback"
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://auth.confirmit.com/login/logincallback", validationRules); // expected true
@@ -109,6 +149,9 @@ namespace AssessmentTests
         [TestMethod]
         public void UriValidator_literalRegexUri_pathWithSeveralFolder_true()
         {
+            string[] validationRules = new[]{
+           @"@https://([\w-]+\.)*confirmit.com/*/logincallback"
+           };
 
             var validator = new UriValidator();
             var isValid = validator.Validate("https://app.confirmit.com/auth/folder/logincallback", validationRules); // expected true
@@ -116,24 +159,46 @@ namespace AssessmentTests
             Assert.AreEqual(true, isValid);
         }
 
-        [TestMethod]
-        public void UriValidator_literalRegexUri_withoutSubdomen_true()
-        {
+        // TODO: is uri without domain
+        //[TestMethod]
+        //public void UriValidator_literalRegexUri_withoutSubdomen_true()
+        //{
+        //    string[] validationRules = new[]{
+        //   @"@https://([\w-]+\.)*confirmit\.com\/logincallback"
+        //   };
 
-            var validator = new UriValidator();
-            var isValid = validator.Validate("https://confirmit.com/logincallback", validationRules); // expected true
+        //    var validator = new UriValidator();
+        //    var isValid = validator.Validate("https://confirmit.com/logincallback", validationRules); // expected true
 
-            Assert.AreEqual(true, isValid);
-        }
+        //    Assert.AreEqual(true, isValid);
+        //}
 
         [TestMethod]
         public void UriValidator_literalRegexUri_wrongdomain_false()
         {
-
+            string[] validationRules = new[]{
+           @"@https://([\w-]+\.)*confirmit\.com\/logincallback"
+           };
             var validator = new UriValidator();
             var isValid = validator.Validate("https://www.confirmit.org/logincallback", validationRules); // expected false
 
             Assert.AreEqual(false, isValid);
+        }
+
+        [TestMethod]
+        public void UriValidator_setOfRules_true()
+        {
+            string[] validationRules = new[]{
+           "https://www.confirmit.com/webapp/logincallback", // absolute rule - this url is allowed
+        "https://*.confirmit.com/*/logincallback", // wildcard rule
+        "https://*.confirmit.com/webapp/login", // wildcard rule
+        @"@https://([\w-]+\.)*confirmit\.com\/logincallback" // literal regex rule	
+           };
+
+            var validator = new UriValidator();
+            var isValid = validator.Validate("https://www.confirmit.com/webapp/logincallback", validationRules); // expected true
+
+            Assert.AreEqual(true, isValid);
         }
     }
 }
