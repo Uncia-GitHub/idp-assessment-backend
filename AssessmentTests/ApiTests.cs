51f846bba0046f2a9425a6ad059a4ca63beda90b
maryna.banova <maryna.banova@gmail.com>
2020-08-10 19:34:09 +0200

    -- add unit tests for calling Api with and without token

--- AssessmentTests/ApiTests.cs
+++ AssessmentTests/ApiTests.cs
@@ -1,12 +1,27 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Api;
+using System.Net.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using IdentityModel.Client;
 
 namespace AssessmentTests
 {
     [TestClass]
     public class ApiTests
     {
+        private readonly HttpClient _client;
+
+        public ApiTests()
+        {
+            var server = new TestServer(new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<Startup>());
+            _client = server.CreateClient();
+        }
+
         [TestMethod]
         public async Task CallUnsecureApi_NoAuth_Ok()
         {
@@ -22,6 +37,12 @@ namespace AssessmentTests
             // var response = await client.GetAsync(url);
 
             // Assert that response.StatusCode matches expectedStatus
+
+            var response = await _client.GetAsync("http://localhost:5000/api/values");
+
+
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual(expected, response.StatusCode);
         }
 
         [TestMethod]
@@ -40,6 +61,29 @@ namespace AssessmentTests
             // var response = await client.GetAsync(url);
 
             // Assert that response.StatusCode matches expectedStatus
+
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://demo.identityserver.io/.well-known/openid-configuration");
+
+
+            //request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "m2m",
+                ClientSecret = "secret",
+                Scope = "api"
+            });
+
+            // call api
+            _client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await _client.GetAsync("http://localhost:5000/api/securevalues");
+
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual(expectedStatus, response.StatusCode);
         }
+
     }
 }
